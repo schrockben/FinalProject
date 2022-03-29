292702eb68fbb1cbd8a2c016d1b1848ee5aad9d2
Ben Schrock <schrock.ben@gmail.com>
2022-03-29 16:02:30 -0400

    Starting the page

--- app.py
+++ app.py
@@ -1,4 +1,29 @@
 import os
 import sys
-import cs50
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+import datetime
 
+# Configures app
+app = Flask(__name__)
+
+# To be done later setting permanent session
+# session.permanent = True
+
+# To be done later Set session to 1 day
+# app.permanent_session_lifetime = datetime.timedelta(days=1)
+
+
+# Auto reload Templates
+app.config["TEMPLATES_AUTO_RELOAD"] = True 
+
+# Configure CS50 library to use SQlite database
+db = SQL("sqlite:///moviesmaster.db")
+
+# Set up index page
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
