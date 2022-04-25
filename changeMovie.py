1e5f54bb5d96806133a824f6cedd65536f29d4e1
Ben Schrock <schrock.ben@gmail.com>
2022-04-25 15:58:34 -0400

    date change for testing

--- changeMovie.py
+++ changeMovie.py
@@ -2,6 +2,6 @@ from flask import Flask, flash, redirect, render_template
 import datetime
 
 todaysDate = datetime.date.today()
-startDate = datetime.date(2022, 4, 17)
+startDate = datetime.date(2022, 4, 16)
 difference = todaysDate - startDate
 x = int(difference.days)
